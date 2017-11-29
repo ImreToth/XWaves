e2782bcc596b56b0ab0265a3f087ee33e5768849
harsanyi <harsanyi@windowslive.com>
2017-11-29 20:44:55 +0100

    GameCreate -> delete and add monsters

--- frontend/src/main/frontend/src/app/_models/Monster.ts
+++ frontend/src/main/frontend/src/app/_models/Monster.ts
@@ -8,4 +8,19 @@ export class Monster {
   speed: number;
   cost: number;
   path: string;
+
+  constructor() { }
+
+  /*constructor() {
+    this.name = '';
+    this.attacktype = '';
+    this.attack = 0;
+    this.health = 0;
+    this.stamina = 0;
+    this.defense = 0;
+    this.speed = 0;
+    this.cost = 0;
+    this.path = '';
+  }*/
+
 }
--- frontend/src/main/frontend/src/app/games/create/create.component.ts
+++ frontend/src/main/frontend/src/app/games/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CyclopaediaService} from '../../_services/cyclopaedia.service';
+import {Monster} from '../../_models/Monster';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  bank = 150;
+  target: Monster;
+  placeholder: Monster;
+  monsters: Monster[];
+  array: number[];
+  N = 100; // mezők száma
+  targetIndex: number;
+  createMatrix= [ new Monster() ];
+  constructor(private cyclopediaService: CyclopaediaService) {
   }
 
+  ngOnInit() {
+    this.placeholder = {name: '', attack: 0, attacktype: '', cost: 0, defense: 0, health: 0, speed: 0, stamina: 0, path: ''};
+    this.monsters = this.cyclopediaService.getMonsters();
+    this.target = this.monsters[0];
+    for (let i = 0; i < 100; i++) {
+      this.createMatrix[i] = this.placeholder;
+    }
+    this.array = Array.apply(null, {length: this.N}).map(Number.call, Number); // mezőgenerátor
+  }
+
+  targetClick(num: number) {
+    if (this.createMatrix[num] !== this.placeholder ) {
+      this.bank += this.createMatrix[num].cost;
+      this.createMatrix[num] = this.placeholder;
+    }
+    if (this.bank - this.target.cost >= 0 && this.createMatrix[num] === this.placeholder) {
+    console.log(num);
+    this.bank -= this.target.cost;
+    this.targetIndex = num;
+    this.createMatrix[this.targetIndex] = this.target;
+    console.log(this.createMatrix); } else {console.log('no money haver'); }
+  }
+  targetDbClick(num: number) {
+    this.bank += this.createMatrix[num].cost;
+    this.createMatrix[num] = this.placeholder;
+  }
 }
