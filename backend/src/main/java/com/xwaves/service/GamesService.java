ed7407d4abd4a42f49435b99e33a9a2932997da3
Unknown <timre96@gmail.com>
2017-12-01 11:28:03 +0100

    JoinPlayer method

--- backend/src/main/java/com/xwaves/service/GamesService.java
+++ backend/src/main/java/com/xwaves/service/GamesService.java
@@ -37,6 +37,26 @@ public class GamesService {
         game.setNextPlayer(nextPlayer);
         gamesRepository.save(game);
     }
-        
-        
+    
+    public int joinPlayer(String name){
+        Games game=getByName(name);
+        if(game.getPlayer1()== null){
+            gamesRepository.delete(game);
+            game.setPlayer1(name);
+            gamesRepository.save(game);
+            return 1;
+        }else if(game.getPlayer2()== null){
+            gamesRepository.delete(game);
+            game.setPlayer2(name);
+            gamesRepository.save(game);
+            return 2;
+        }else if(game.getPlayer3()== null){
+            gamesRepository.delete(game);
+            game.setPlayer3(name);
+            gamesRepository.save(game);
+            return 3;
+        }
+        return 0;          
     }
+        
+}
