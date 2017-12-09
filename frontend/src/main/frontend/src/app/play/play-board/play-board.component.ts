0b693d1d20099b8b6e55e8401acb08691f1670c9
harsanyi <harsanyi@windowslive.com>
2017-12-09 22:11:33 +0100

    Rules

--- frontend/src/main/frontend/src/app/play/play-board/play-board.component.ts
+++ frontend/src/main/frontend/src/app/play/play-board/play-board.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./play-board.component.css']
 })
 export class PlayBoardComponent implements OnInit {
-
-  constructor() { }
+  array: number[];
+  N = 100; // mezők száma
+  constructor() {
+    this.array = Array.apply(null, {length: this.N}).map(Number.call, Number); // mezőgenerátor
+  }
 
   ngOnInit() {
   }
+
 
 }
--- frontend/src/main/frontend/src/app/rules/rules.component.ts
+++ frontend/src/main/frontend/src/app/rules/rules.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rules.component.css']
 })
 export class RulesComponent implements OnInit {
-  array: number[];
-  N = 100; // mezők száma
   constructor() {
-    this.array = Array.apply(null, {length: this.N}).map(Number.call, Number); // mezőgenerátor
   }
 
   ngOnInit() {
